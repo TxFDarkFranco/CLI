547534c00e2ace0502d9be6e738b6bd7fe38e12a
RobertZapatal-12 <yarelzapatal@gmail.com>
2026-08-13 18:22:46 -0400

    Add property methods for name in Suppliers class

--- repositories/suppliers.py
+++ repositories/suppliers.py
@@ -9,5 +9,18 @@ class Suppliers:
         self.product = product
         self.price = price
 
+        @property
+        def name(self):
+            return "XXX" + self._name[-3:]
+        
+        @name.setter
+        def name(self, new_name):
+            self._name = new_name
+        
+        @name.deleter
+        def name(self):
+            del self._name
+            print("Name deleted")
+            
     def __str__(self):
         return f"{self.id}, {self.name}, {self.contact}, {self.gmail}, {self.product}, {self.price}"
