8fa65e4095ae80ded9010412926c42e1fcff462a
Luis Gonzalez <166937014+Luisjg02@users.noreply.github.com>
2026-08-12 20:55:25 -0400

    Guardar cambios locales antes de pull

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from menu import menu
+from app.menu import menu
 
 menu()
 
--- app/menu.py
+++ app/menu.py
@@ -1,42 +1,46 @@
-from managers.warehouse_manager import WarehouseManager
 from managers.suppliers_manager import SuppliersManager
-
-productm = WarehouseManager()
-supplierstm = SuppliersManager()
+from managers.warehouse_manager import WarehouseManager
 
 
 def menu():
-    print("=" * 25)
-    print("Warehouse & Logistics Management System")
-    print("=" * 25)
-    print("1.Insertar productos")
-    print("2.Ver productos")
-    print("3.Crear alamacen")
-    print("4.Ver alamcenes")
-    print("5.Crear Suplidores")
-    print("6.Ver Suplidores")
+    productm = WarehouseManager()
+    supplierstm = SuppliersManager()
 
     while True:
-        opcion = input("Elige una opcion: ")
+        print("\n" + "=" * 25)
+        print("Warehouse & Logistics Management System")
+        print("=" * 25)
+        print("1. Insertar productos")
+        print("2. Ver productos")
+        print("3. Crear almacen")
+        print("4. Ver almacenes")
+        print("5. Crear suplidores")
+        print("6. Ver suplidores")
+        print("7. Editar almacenes")
+        print("8. Eliminar almacenes")
+        print("9. Salir")
+
+        opcion = input("\nElige una opcion: ")
+
         match opcion:
             case "1":
                 productm.insertar_productos()
-                continue
-
             case "2":
                 productm.ver_productos()
-                continue
-
             case "3":
                 productm.crear_almacen()
-                continue
-
             case "4":
                 productm.ver_almacenes()
-                continue
-
             case "5":
                 supplierstm.crear_suppliers()
-
             case "6":
                 supplierstm.ver_suppliers()
+            case "7":
+                productm.editar_almacenes()
+            case "8":
+                productm.eliminar_almacenes()
+            case "9":
+                print("Programa finalizado")
+                break
+            case _:
+                print("Opción no válida. Intenta de nuevo.")
