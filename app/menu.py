a6c90111979b4e907b64d08d4cb29ec42123aef5
Luis Gonzalez <166937014+Luisjg02@users.noreply.github.com>
2026-08-12 21:13:37 -0400

    hola

--- app/menu.py
+++ app/menu.py
@@ -1,6 +1,5 @@
 from managers.suppliers_manager import SuppliersManager
 from managers.warehouse_manager import WarehouseManager
-from managers.utils import guardar_datos
 
 
 def menu():
@@ -30,13 +29,20 @@ def menu():
                 productm.ver_productos()
             case "3":
                 productm.crear_almacen()
-                continue
             case "4":
                 productm.ver_almacenes()
             case "5":
                 supplierstm.crear_suppliers()
             case "6":
                 supplierstm.ver_suppliers()
-
             case "7":
-                guardar_datos()
+                productm.editar_almacenes()
+            case "8":
+                productm.eliminar_almacenes()
+            case "9":
+                print("Programa finalizado")
+                break
+            case _:
+                print("Opción no válida. Intenta de nuevo.")
+
+
